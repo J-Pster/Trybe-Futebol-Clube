2746e94965c5a0146b3ec33337ae61f4c378407a
João Pster <joaopsterdev@gmail.com>
2022-10-05 16:52:32 -0300

    Adicionado todas as interfaces e models para o Match

---
+++ app/backend/src/interfaces/match.interface.ts
@@ -0,0 +1,24 @@
+// O Score Básico
+
+export interface IMScore {
+  homeTeamGoals: number;
+  awayTeamGoals: number;
+}
+
+// O Input de dados
+export interface IMInput extends IMScore{
+  homeTeam: number;
+  awayTeam: number;
+}
+
+// A partida em sí, que tem o score e o input completinho
+export interface IMatch extends IMInput {
+  id: number;
+  inProgress: boolean
+  teamHome: {
+    teamName: string
+  };
+  teamAway: {
+    teamName: string
+  }
+}
---
+++ app/backend/src/models/match.model.ts
@@ -0,0 +1,52 @@
+import Match from '../database/models/Match';
+import Team from '../database/models/Team';
+import { IMatch, IMInput, IMScore } from '../interfaces/match.interface';
+
+export default class UserModel {
+  constructor(private _model: typeof Match = Match) {}
+
+  public async findAll(): Promise<IMatch[] | null> {
+    const result = await this._model.findAll({
+      include: [
+        { model: Team, as: 'teamHome', attributes: ['teamName'] },
+        { model: Team, as: 'teamAway', attributes: ['teamName'] },
+      ]
+    });
+    return result;
+  }
+
+  public async findByProgress(progress: boolean): Promise<IMatch[] | null> {
+    const result = await this._model.findAll({
+      where: { progress },
+      include: [
+        { model: Team, as: 'teamHome', attributes: ['teamName'] },
+        { model: Team, as: 'teamAway', attributes: ['teamName'] },
+      ]
+    });
+    return result;
+  };
+
+  public async create(match: IMInput): Promise<IMatch> {
+    const result = await this._model.create({
+      ...match,
+      inProgress: true,
+    });
+    return result;
+  }
+
+  public async finish(id: number): Promise<number> {
+    const [result] = await this._model.update(
+      { inProgress: false },
+      { where: { id } }
+    );
+    return result;
+  }
+
+  public async updateScore(id: number, score: IMScore): Promise<number> {
+    const [result] = await this._model.update(
+      { ...score },
+      { where: { id } }
+    );
+    return result;
+  }
+}
